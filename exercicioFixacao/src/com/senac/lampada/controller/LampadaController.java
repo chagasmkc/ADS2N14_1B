fbb1c803778f308f0fb34b4f67fe559254ee83c9
chagasmkc <chagas.mkc@gmail.com>
2014-03-26 11:02:30 -0300

    Exercicios de fixacao.

---
+++ exercicioFixacao/src/com/senac/lampada/controller/LampadaController.java
@@ -0,0 +1,20 @@
+package com.senac.lampada.controller;
+
+import java.util.Scanner;
+
+import com.senac.lampada.molel.LampadaModel;
+
+
+
+
+public class LampadaController {
+	
+	
+	Scanner ler = new Scanner(System.in);
+	private LampadaModel lampada = new LampadaModel();
+
+	
+	String acao = ler.next();
+	
+	
+}
---
+++ exercicioFixacao/src/com/senac/lampada/molel/LampadaModel.java
@@ -0,0 +1,26 @@
+package com.senac.lampada.molel;
+
+public class LampadaModel {
+	
+	private String lampada;
+	private String acao;
+	
+	
+	public String getLampada() {
+		return lampada;
+	}
+	
+	public String getAcao() {
+		return acao;
+	}
+
+	
+	public void setLampada(String lampada) {
+		this.lampada = lampada;
+	}
+	
+	public void setAcao(String acao) {
+		this.acao = acao;
+	}
+	
+}
