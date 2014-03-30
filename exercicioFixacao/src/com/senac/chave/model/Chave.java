1c27f053420b5fcf10231252927b54c846b26f5e
chagasmkc <chagas.mkc@gmail.com>
2014-03-30 18:32:30 -0300

    Alteracoes exercicio de fixacao.

---
+++ exercicioFixacao/src/com/senac/chave/model/Chave.java
@@ -0,0 +1,18 @@
+package com.senac.chave.model;
+
+public class Chave {
+	
+	private int codigo;
+
+	public Chave(int codigo) {
+		this.codigo = codigo;
+	
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+	
+		
+
+}
--- exercicioFixacao/src/com/senac/porta/model/Porta.java
+++ exercicioFixacao/src/com/senac/porta/model/Porta.java
@@ -2,19 +2,45 @@ package com.senac.porta.model;
 
 public class Porta {
 	
-	private boolean estadoPorta;
+	private boolean aberta; // lista de atributos; alt shift r
+	private boolean trancada;
+	private int codigo;
+	
+	//construtor;
+	
+	public Porta(boolean estadoPorta, boolean fechadura, int codigo) {
+		this.aberta = estadoPorta;
+		this.trancada = fechadura;
+		this.codigo = codigo;
+	}
+		
+	//metodos acessores
 	
 	public boolean getEstadoPorta() {
-		return estadoPorta;
+		return aberta;
+	}
+	
+	public boolean getFechadura() {
+		return trancada;
+	}
+	
+	public int getCodigo(){
+		return codigo;
 	}
 	
 	public void setEstadoPorta(boolean estadoPorta) {
-		this.estadoPorta = estadoPorta;
+		this.aberta = estadoPorta;
 	}
 
-	public Porta(boolean estadoPorta) {
-		this.estadoPorta = estadoPorta;
-		estadoPorta = false;
+	public void trancar(int codigo){
+		
+		if (codigo == this.codigo) {
+			this.trancada = true;
+		}
+		
+		this.trancada = false;
+		
 	}
+	
 	
 }
