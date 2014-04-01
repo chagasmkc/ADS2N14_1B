43450fcd6d6794bb2492be83b68ced14a7b56838
chagasmkc <chagas.mkc@gmail.com>
2014-04-01 14:01:10 -0300

    Add Classe Investimento.

---
+++ Banco/src/com/senac/banco/model/Investimento.java
@@ -0,0 +1,19 @@
+package com.senac.banco.model;
+
+public class Investimento {
+	
+	private int data;
+	private double taxa;
+
+	
+	public int getData() {
+		return data;
+	}
+
+	public double getTaxa() {
+		return taxa;
+	}
+
+}
+
+
